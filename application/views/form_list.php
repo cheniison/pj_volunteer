bac9d132d0bcef07b8fc3704e6f01052fcd5bb24
cheniison <cheniison@163.com>
2016-08-09 17:20:47 +0800

    界面友好

--- application/views/form_list.php
+++ application/views/form_list.php
@@ -19,7 +19,7 @@
 
 <div class="box box-default">
     <div class="box-header with-border">
-        条件搜索
+        条件搜索(按住ctrl键可以多选)
         <div class="box-tools pull-right">
         <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
         <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
